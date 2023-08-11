bb7056ae7937f411363c5a9f64f94a48f0ab300d
beken0w <beken0w@yandex.ru>
2023-08-11 15:44:36 +0600

    added info and empty cycles

--- testproject/news/views.py
+++ testproject/news/views.py
@@ -3,21 +3,26 @@ from news.models import News
 
 
 def main(request):
-    news = News.objects.all()[:10]
-    showbuss = News.objects.filter(category='showbuss')
-    world = News.objects.filter(category='world')
-    context = {
-        'news': news,
-        'big_main_news': news[0],
-        'big_small_news': news[1:3],
-        'side_news': news[3:7],
-        'circles': news[1:5],
-        'horizontal_demo': showbuss,
-        'horizontal_plus': world,
-        'horizontal_left': world[0],
-        'horizontal_right': world[1:4]
-        
-    }
+    if News.objects.count() >= 10:
+        news = News.objects.all()[:10]
+        showbuss = News.objects.filter(category='showbuss')
+        world = News.objects.filter(category='world')
+        context = {
+            'news': news,
+            'big_main_news': news[0],
+            'big_small_news': news[1:3],
+            'side_news': news[3:7],
+            'circles': news[1:5],
+            'horizontal_demo': showbuss,
+            'horizontal_plus': world,
+            'horizontal_left': world[0],
+            'horizontal_right': world[1:4],
+            'has_news': True
+        }
+    else:
+        context = {
+            'has_news': False
+        }
     return render(request, 'includes/news.html', context)
 
 
@@ -43,3 +48,20 @@ def category(request, category=None):
         'list_news': list_news,
     }
     return render(request, 'includes/list_news.html', context)
+
+
+def info(request, type='about'):
+    if type.lower() == 'about':
+        text = "Здесь информация о Нас"
+    elif type.lower() == 'price':
+        text = "Здесь информация о Рекламе"
+    elif type.lower() == 'rule':
+        text = "Здесь информация о Правилах"
+    elif type.lower() == 'conf':
+        text = "Здесь информация о Политике Конфиденциальности"
+    else:
+        text = "Такой страницы нет"
+    context = {
+        'text': text
+    }
+    return render(request, "includes/info.html", context)
